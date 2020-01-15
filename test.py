08d2956b78aad34372f84071f115a54e8dbc34ea
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-01-15 22:24:58 +0100

    First data exploration

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
-test = pd.read_parquet(os.path.join('.', 'data', 'bengaliai-cv19', 'test_image_data_0.parquet'))
+test = pd.read_parquet(os.path.join('.', 'data', 'bengaliai-cv19', 'train_image_data_0.parquet'))
 print(test.head(10))
-
+print(test.shape)
+print(test.columns)
