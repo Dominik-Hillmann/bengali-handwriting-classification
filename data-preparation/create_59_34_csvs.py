e7d363ddbb0328eec289e16ee1e23aea29f3ce18
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-01-20 22:09:05 +0100

    Started data transformation

--- data-preparation/create_59_34_csvs.py
+++ data-preparation/create_59_34_csvs.py
@@ -3,23 +3,28 @@ import os
 import sys
 # External libraries
 import pandas as pd
-# Self-written
-# sys.path.insert(0, os.path.join(
-#     'C:', 'Users', 'Dominik USER', 'Repositories', 'bengali-handwriting-classification', 'utils', 'path_helpers.py'
-# ))
-# sys.path.append('..')
-from '..'  import utils
-# import utils.path_helpers
+import cv2
 
 
+DATA_PATH = os.path.join('data', 'bengaliai-cv19')
+SIZE = 32
 
 def main():
-    
-    print(1)
+    train_targets = pd.read_csv(os.path.join(DATA_PATH, 'train.csv'))
+    train_data = []
+    lower_res_data = train_targets[['image_id', 'grapheme_root', 'vowel_diacritic', 'consonant_diacritic']]
 
+    for num_batch in range(4):
+        train_data = pd.read_parquet(os.path.join(DATA_PATH, f'train_image_data_{num_batch}.parquet'))
 
+        for i in list(train_data.index):
+            high_res_pic = train_data.iloc[i][1:].to_numpy(dtype = float)
+            high_res_pic /= 255.0
+            high_res_pic = 1.0 - high_res_pic
+            low_res_pic = cv2.resize(high_res_pic, dsize = (SIZE, SIZE), interpolation = cv2.INTER_CUBIC)
+            low_res_pic = low_res_pic.reshape((SIZE ** 2, ))
 
-
+    # Target: Make new parquet file with y and X in lower resolution
 
 if __name__ == '__main__':
     main()
