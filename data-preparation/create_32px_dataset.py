42ce858e1e2701e462cfde3de0557c274e8f2319
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-01-23 22:47:23 +0100

    Appending to data frame row by row is too slow

--- data-preparation/create_32px_dataset.py
+++ data-preparation/create_32px_dataset.py
@@ -1,34 +1,42 @@
 # Python libraries
 import os
+
 # External libraries
 import pandas as pd
 import cv2
 from tqdm import tqdm
+
 # Important constants
 DATA_PATH = os.path.join('data', 'bengaliai-cv19')
 SIZE = 32
 PX_COLS = [f'px_{i}' for i in range(SIZE ** 2)]
 ORIG_DIMS = (137, 236)
+BATCH_SIZE = 50210
 
 
 def main():
     train_y_low_res, train_X_low_res = load_meta_data()
-    px_lists = [[] for _ in range(SIZE ** 2)]
+    # px_lists = [[]] * (SIZE ** 2)
+    collection = {}
     print(train_y_low_res.head(5))
     print(train_X_low_res.head(5))
 
     for num_batch in range(1):
-        print(f'Loading batch {num_batch + 1} of 4...')
+        print(f'Loading batch {num_batch + 1} of 4...')        
         train_X_batch = pd.read_parquet(os.path.join(DATA_PATH, f'train_image_data_{num_batch}.parquet'))
 
         for i in tqdm(list(train_X_batch.index)):
-            low_res_pic = get_observation(train_X_batch, i)
-            px_lists = append_to_px_lists(low_res_pic, px_lists)
-        
-        px_lists = pd.DataFrame(px_lists, columns = PX_COLS)
-        train_X_low_res = train_X_low_res.append(px_lists)
+            inter_batch_index = num_batch * BATCH_SIZE + i
+            low_res_flat_pic = get_observation(train_X_batch, i)
+            frame_index = f'Train_{inter_batch_index}'
+            collection[frame_index] = low_res_flat_pic
+            # train_X_low_res.loc[frame_index] = low_res_flat_pic
+            # ineffizient, jedes einzelne Element anhängen wird langsamer je größer der DataFrame
+            # => Listen in Dict, mit key
 
-        print(train_X_low_res.head(10))
+
+        # print(train_X_low_res.head(10))
+        print(pd.DataFrame(list(collection.values()), index = list(collection.keys())))
 
 
 def load_meta_data():
@@ -47,7 +55,7 @@ def get_observation(train_X_batch, i):
     low_res_pic = cv2.resize(high_res_pic, dsize = (SIZE, SIZE), interpolation = cv2.INTER_CUBIC)
     low_res_pic = low_res_pic.reshape((SIZE ** 2, ))
 
-    return low_res_pic
+    return low_res_pic.tolist()
 
 
 def append_to_px_lists(flat_low_res_img, px_lists):
