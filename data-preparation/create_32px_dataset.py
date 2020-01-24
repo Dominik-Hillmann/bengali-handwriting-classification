717c3d84d53664d03b3fc97cafed52fdfbc24d26
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-01-24 22:19:29 +0100

    Finished 32px data generation

--- data-preparation/create_32px_dataset.py
+++ data-preparation/create_32px_dataset.py
@@ -14,40 +14,60 @@ ORIG_DIMS = (137, 236)
 BATCH_SIZE = 50210
 
 
-def main():
-    train_y_low_res, train_X_low_res = load_meta_data()
-    # px_lists = [[]] * (SIZE ** 2)
-    collection = {}
-    print(train_y_low_res.head(5))
-    print(train_X_low_res.head(5))
+def main() -> None:
+    train_y_low_res = load_y()
+    converted_batches = []
 
-    for num_batch in range(1):
+    for num_batch in range(4):
         print(f'Loading batch {num_batch + 1} of 4...')        
         train_X_batch = pd.read_parquet(os.path.join(DATA_PATH, f'train_image_data_{num_batch}.parquet'))
 
+        collection = {}
         for i in tqdm(list(train_X_batch.index)):
             inter_batch_index = num_batch * BATCH_SIZE + i
             low_res_flat_pic = get_observation(train_X_batch, i)
             frame_index = f'Train_{inter_batch_index}'
             collection[frame_index] = low_res_flat_pic
-            # train_X_low_res.loc[frame_index] = low_res_flat_pic
-            # ineffizient, jedes einzelne Element anhängen wird langsamer je größer der DataFrame
-            # => Listen in Dict, mit key
+
+        converted_batches.append(pd.DataFrame(
+            list(collection.values()),
+            index = list(collection.keys()),
+            columns = PX_COLS
+        ))
+
+    save_converted_pics(train_y_low_res, converted_batches)
+    
 
 
-        # print(train_X_low_res.head(10))
-        print(pd.DataFrame(list(collection.values()), index = list(collection.keys())))
+def load_y() -> pd.DataFrame:
+    """Loads the file containing the labels.
+    
+    Returns:
+        pd.DataFrame -- The `csv` file load into a dataframe.
+    """
+
+    train_y = pd.read_csv(
+        os.path.join(DATA_PATH, 'train.csv'),
+        index_col = 0
+    )
+    train_y_low_res = train_y[['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']]
+    # train_X_low_res = pd.DataFrame(columns = PX_COLS)
+
+    return train_y_low_res#, train_X_low_res
 
 
-def load_meta_data():
-    train_y = pd.read_csv(os.path.join(DATA_PATH, 'train.csv'))
-    train_y_low_res = train_y[['image_id', 'grapheme_root', 'vowel_diacritic', 'consonant_diacritic']]
-    train_X_low_res = pd.DataFrame(columns = PX_COLS)
+def get_observation(train_X_batch: pd.DataFrame, i: int) -> list:
+    """Get a formatted observation number `i` from this batch.
+    It will a flattened and lower resolution version of the original picture.
+    
+    Arguments:
+        train_X_batch {pd.DataFrame} -- The batch containing the original data.
+        i {int} -- Observation number.
+    
+    Returns:
+        list -- The flattened and downscaled observation.
+    """
 
-    return train_y_low_res, train_X_low_res
-
-
-def get_observation(train_X_batch, i):
     high_res_pic = train_X_batch.iloc[i][1:].to_numpy(dtype = float)
     high_res_pic /= 255.0
     high_res_pic = 1.0 - high_res_pic
@@ -58,16 +78,21 @@ def get_observation(train_X_batch, i):
     return low_res_pic.tolist()
 
 
-def append_to_px_lists(flat_low_res_img, px_lists):
-    flat_low_res_img = list(flat_low_res_img)
-    for i in range(len(flat_low_res_img)):
-        px_lists[i].append(flat_low_res_img[i])
+def save_converted_pics(train_y: pd.DataFrame, batches_list: list) -> None:
+    """Save the converted data to a `.parquet` file.
+    
+    Arguments:
+        train_y {pd.DataFrame} -- The labels of the observations
+        batches_list {list} -- A list of the four converted batches.
+    """
 
-    return px_lists
-
-
-def add_obs_low_res_X(low_res_X):
-    raise NotImplementedError
+    train_X_low_res = pd.concat(batches_list, axis = 0, ignore_index = False)
+    train_X_low_res = pd.concat([train_y, train_X_low_res], axis=1, sort=False)
+    train_X_low_res.to_parquet(
+        os.path.join(DATA_PATH, '32by32-y-and-X.parquet'),
+        engine = 'pyarrow',
+        index = True
+    )
 
 
 if __name__ == '__main__':
